e847c2cbf2e5596e821df12e902799cacd4ed613
Renato Carvalho <renato_luizcarvalho@hotmail.com>
2016-09-22 16:10:58 -0300

    Example of SignalR

---
+++ WebApplicationNotificationSR/NotificationHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationNotificationSR
+{
+    public class NotificationHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.SendNotification(name, message);
+        }
+    }
+}
